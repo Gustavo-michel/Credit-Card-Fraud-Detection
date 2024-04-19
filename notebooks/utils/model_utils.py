17d8ce8f0705222f53f092d51bd90fc8fd7449b3
Gustavo <gustavomichelads@gmail.com>
2024-04-18 23:16:11 -0300

    Validation clf test

--- notebooks/utils/model_utils.py
+++ notebooks/utils/model_utils.py
@@ -2,11 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score, log_loss, make_scorer, f1_score, precision_score, recall_score
+from sklearn.metrics import roc_auc_score, roc_curve, log_loss, make_scorer, f1_score
+from sklearn import metrics
 
-import lightgbm as lgb
 from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 
@@ -21,7 +20,6 @@ classifiers = [
     RandomForestClassifier(),
     AdaBoostClassifier(algorithm='SAMME'),
 ]
-
 def preprocessor(X):
     '''
      performs preprocessing
@@ -46,9 +44,9 @@ def best_params_models():
     [{'n_estimators': [50, 100, 200], 'criterion': ('gini', 'entropy', 'log_loss')}], # RandomForestClassifier
     [{'n_estimators': [25, 50, 100,], 'learning_rate':  (0.1, 0.5, 1.0)}], # AdaBoostClassifier
     #[{}], # LightGBM
-    scores = {'accuracy' :make_scorer(accuracy_score),
-        'recall'   :make_scorer(recall_score),
-        'precision':make_scorer(precision_score),
+    scores = {'accuracy' :make_scorer(metrics.accuracy_score),
+        'recall'   :make_scorer(metrics.recall_score),
+        'precision':make_scorer(metrics.precision_score),
         'f1'       :make_scorer(f1_score)}
     best_params = []
     for classifier in classifiers:
@@ -67,7 +65,7 @@ def validation_clf_models(X_train, X_test, y_train, y_test):
      performs model validation with scores.
     '''
     for classifier in classifiers:
-        pipe = Pipeline(steps=[("preprocessor", preprocessor(X_train=X_train)), ("classifier", classifier)])
+        pipe = Pipeline(steps=[("preprocessor", preprocessor(X=X_train)), ("classifier", classifier)])
         
         pipe.fit(X_train, y_train)
         predict_train = pipe.predict(X_train)
@@ -75,13 +73,17 @@ def validation_clf_models(X_train, X_test, y_train, y_test):
 
         print(classifier)
         print(' ')
-        print(f"Roc Score train: {roc_auc_score(y_train, predict_train):.3f}")
+        print(f"F1 Score train: {f1_score(y_train, predict_train):.3f}")
+        print(f"Accuracy train: {metrics.accuracy_score(y_train, predict_train):.3f}")
+        print(f"Recall train: {metrics.recall_score(y_train, predict_train):.3f}")
+        print(f"Precision train: {metrics.precision_score(y_train, predict_train):.3f}")
         print(f"Log loss train: {log_loss(y_train, predict_train):.3f}")
-        print(f"Roc Score train: {f1_score(y_train, predict_train):.3f}")
         print('- '*30)
-        print(f"Roc Score test: {roc_auc_score(y_test, predict_test):.3f}")
+        print(f"F1 Score test: {f1_score(y_test, predict_test):.3f}")
+        print(f"Accuracy test: {metrics.accuracy_score(y_test, predict_test):.3f}")
+        print(f"Recall test: {metrics.recall_score(y_test, predict_test):.3f}")
+        print(f"Precision test: {metrics.precision_score(y_test, predict_test):.3f}")
         print(f"Log loss test: {log_loss(y_test, predict_test):.3f}")
-        print(f"Roc Score test: {f1_score(y_test, predict_test):.3f}")
         print('-'*50)
 
 def plot_validation_clf_models(X_train, X_test, y_train, y_test):
@@ -89,7 +91,7 @@ def plot_validation_clf_models(X_train, X_test, y_train, y_test):
      Plot Score of models with ROC Curve
     '''
     for classifier in classifiers:
-        pipe = Pipeline(steps=[("preprocessor", preprocessor(X_train=X_train)), ("classifier", classifier)])
+        pipe = Pipeline(steps=[("preprocessor", preprocessor(X=X_train)), ("classifier", classifier)])
         
         pipe.fit(X_train, y_train)
         y_pred = pipe.predict_proba(X_test)[:, 1]
@@ -100,8 +102,8 @@ def plot_validation_clf_models(X_train, X_test, y_train, y_test):
     plt.xlabel('Falso Positivo')
     plt.ylabel('verdadeiro Positivo')
     plt.title('Comparando modelos ROC-CURVE')
-    plt.savefig("img/Comparando modelos ROC-CURVE")
     plt.legend(loc="lower right")
+    plt.savefig("img/Comparando modelos ROC-CURVE")
 
 
 def evaluate():
