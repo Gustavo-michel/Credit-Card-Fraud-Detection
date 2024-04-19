aa9f4be1a2bff0f3810ed03f542a8636f19f03b4
Gustavo <gustavomichelads@gmail.com>
2024-04-19 15:56:43 -0300

    Model search

--- notebooks/utils/model_utils.py
+++ notebooks/utils/model_utils.py
@@ -34,7 +34,6 @@ def preprocessor(X):
 
 
 def best_params_models():
-    # SVC=False, LR=False, RandomForest=False, Adaboost=False
     '''
         defines the best hyperparameters for the model using Grid search.
     '''
@@ -44,14 +43,18 @@ def best_params_models():
     [{'n_estimators': [50, 100, 200], 'criterion': ('gini', 'entropy', 'log_loss')}], # RandomForestClassifier
     [{'n_estimators': [25, 50, 100,], 'learning_rate':  (0.1, 0.5, 1.0)}], # AdaBoostClassifier
     #[{}], # LightGBM
+
     scores = {'accuracy' :make_scorer(metrics.accuracy_score),
         'recall'   :make_scorer(metrics.recall_score),
         'precision':make_scorer(metrics.precision_score),
         'f1'       :make_scorer(f1_score)}
+    
+    print("0 = SVC, 1 = Logistic Regression, 2 = Random Forest, 4 = AdaBoost")
+    select_clf = int(input("Selecione o Classificador: "))
+
+    grid = GridSearchCV(estimator=classifiers[select_clf], param_grid=parameters[select_clf], scoring=scores)
     best_params = []
-    for classifier in classifiers:
-       grid = GridSearchCV(estimator=classifier, param_grid=parameters[classifier], scoring=scores)
-       best_params.append(grid.best_params_)
+    best_params.append(grid.best_params_)
 
     print(pd.DataFrame(grid.cv_results_)[['params',
                                   'mean_test_recall',
@@ -107,5 +110,7 @@ def plot_validation_clf_models(X_train, X_test, y_train, y_test):
 
 
 def evaluate():
+    # print(confusion_matrix(Y_test, predictions))
+    # print(classification_report(Y_test, predictions))
     pass
        
