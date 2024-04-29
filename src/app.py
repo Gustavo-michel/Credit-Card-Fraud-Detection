d362ed0b866762d5120cb26c0ed172cdaaeabcf7
Gustavo Michel <gustavomichelads@gmail.com>
2024-04-29 13:04:14 -0300

    Make flask implementation

--- src/app.py
+++ src/app.py
@@ -1,1 +1,38 @@
-import flask
+from flask import Flask, render_template,request, flash
+import pickle
+import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.model_utils import *
+
+app = Flask(__name__)
+secret_key = '7829'
+app.config['SECRET_KEY'] = secret_key
+
+@app.route("/dash")
+def Dash():
+    return render_template('Dash.html')
+
+@app.route("/", methods=['POST', 'GET'])
+def form():
+    correlacao_positiva1 = request.form.get('correlacao_positiva1')
+    correlacao_negativa1 = request.form.get('correlacao_negativa1')
+    correlacao_positiva2 = request.form.get('correlacao_positiva2')
+    correlacao_negativa2 = request.form.get('correlacao_negativa2')
+    if correlacao_positiva1 is not None or correlacao_negativa1 is not None or correlacao_positiva2 is not None or correlacao_negativa2 is not None:
+        X = pd.DataFrame({'Corr1': float(correlacao_positiva1), 'Corr2': float(correlacao_positiva2), 
+                        'Corr3': float(correlacao_negativa1), 'Corr4': float(correlacao_negativa2)}, index=[0])
+
+        X = preprocessor(X=X)
+        with open('notebooks/model/mini_fraud_detection_clf.sav', 'rb') as file:
+            model = pickle.load(file)
+        predict = model.predict(X)
+    return render_template('form.html', result=predict)
+
+@app.route("/model", methods=['POST', 'GET'])
+def model():
+    return render_template('model.html')
+
+if __name__ in '__main__':
+    app.run(debug=True)
