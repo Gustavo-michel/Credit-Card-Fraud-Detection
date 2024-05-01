3600c55fc8ffd3dd299956909a34de03e9cb20f2
Gustavo Michel <gustavomichelads@gmail.com>
2024-05-01 15:38:00 -0300

    Final pages

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request, flash
 import pickle
+import numpy as np
 import pandas as pd
 import sys
 import os
@@ -10,16 +11,17 @@ app = Flask(__name__)
 secret_key = '7829'
 app.config['SECRET_KEY'] = secret_key
 
-@app.route("/dash")
+@app.route("/")
 def Dash():
     return render_template('Dash.html')
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/form", methods=['POST', 'GET'])
 def form():
     correlacao_positiva1 = request.form.get('correlacao_positiva1')
     correlacao_negativa1 = request.form.get('correlacao_negativa1')
     correlacao_positiva2 = request.form.get('correlacao_positiva2')
     correlacao_negativa2 = request.form.get('correlacao_negativa2')
+    predict = np.array([0])
     if correlacao_positiva1 is not None or correlacao_negativa1 is not None or correlacao_positiva2 is not None or correlacao_negativa2 is not None:
         X = pd.DataFrame({'Corr1': float(correlacao_positiva1), 'Corr2': float(correlacao_positiva2), 
                         'Corr3': float(correlacao_negativa1), 'Corr4': float(correlacao_negativa2)}, index=[0])
